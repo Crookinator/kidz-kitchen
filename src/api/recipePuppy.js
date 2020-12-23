e2c217a8637d1a4f21da864a0bb87f81aa8a7611
Crookinator <a_crook@icloud.com>
2020-12-23 17:27:16 -0500

    Corrected http / https issue for the third party api
    was getting an error on index from the third party api saying that its access was blocked. Determined that the issue was because the api call was going to http not https

--- src/api/recipePuppy.js
+++ src/api/recipePuppy.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const generateRecipes = (query) => {
   return axios({
     method: 'GET',
-    url: `http://recipepuppyproxy.herokuapp.com/api/?q=${query}`
+    url: `https://recipepuppyproxy.herokuapp.com/api/?q=${query}`
   })
 }
