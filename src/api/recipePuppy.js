dae526bd1470f50e65016b8066b9537db519f499
Crookinator <a_crook@icloud.com>
2020-12-23 17:39:38 -0500

    commit
    commit

--- src/api/recipePuppy.js
+++ src/api/recipePuppy.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export const generateRecipes = (query) => {
   return axios({
     method: 'GET',
-    url: `https://recipepuppyproxy.herokuapp.com/api/?q=${query}`
+    url: `http://recipepuppyproxy.herokuapp.com/api/?q=${query}`
   })
 }
